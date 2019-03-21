fb7b9ba3b6b46472bff57c3dfe9bb70fe29d3dc9
SwathiBoya <boyaswathi25@gmail.com>
2019-03-21 17:27:44 +0530

    Booking app first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,27 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { ForgotPwdComponent } from './forgot-pwd/forgot-pwd.component';
 
 const routes: Routes = [
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' }
+  {
+    path:'login',
+    component:LoginComponent
+  },
+  {
+    path:'registration',
+    component:RegistrationComponent
+  },
+  {
+    path:'forgot-pwd',
+    component:ForgotPwdComponent
+  },
+  { 
+    path: '', 
+    redirectTo: 'login' ,
+    pathMatch:'full'
+  }
 ];
 @NgModule({
   imports: [
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,16 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MatButtonModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { ForgotPwdComponent } from './forgot-pwd/forgot-pwd.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent,LoginComponent,RegistrationComponent,ForgotPwdComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,BrowserAnimationsModule, MatButtonModule],
   providers: [
     StatusBar,
     SplashScreen,
